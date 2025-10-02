a313efe6e6fca6a13fc2e2b9d8931a9e5a586720
eds1 <99983313+S1l1c1um@users.noreply.github.com>
2025-10-03 00:33:45 +0300

    Update SQLQuery1.sql

--- JeweleryStore-database/SQLQuery1.sql
+++ JeweleryStore-database/SQLQuery1.sql
@@ -1,19 +1,21 @@
+USE JeweleryStore;
+
 CREATE TABLE Discount (
     ID INT IDENTITY(1,1) PRIMARY KEY,
-    Denomination NVARCHAR(20) NOT NULL,
-    size INT NOT NULL CHECK (size >= 0 AND size <= 100)
+    наименование NVARCHAR(20) NOT NULL,
+    размер INT NOT NULL CHECK (размер >= 0 AND размер <= 100)
 );
 
 CREATE TABLE Client (
     ID INT IDENTITY(1,1) PRIMARY KEY,
-    Full_name NVARCHAR(100) NOT NULL,
-    Adres NVARCHAR(100),
-    Telephone NVARCHAR(20)
+    ФИО NVARCHAR(100) NOT NULL,
+    Адрес NVARCHAR(100),
+    телефон NVARCHAR(20)
 );
 
 CREATE TABLE Workshop (
     ID INT IDENTITY(1,1) PRIMARY KEY,
-    Adres NVARCHAR(100) NOT NULL 
+    адрес NVARCHAR(100) NOT NULL 
 );
 
 CREATE TABLE Material (
@@ -22,17 +24,17 @@ CREATE TABLE Material (
 
 CREATE TABLE Jewelery (
     ID INT IDENTITY(1,1) PRIMARY KEY,
-    Product_name NVARCHAR(100) NOT NULL,
-    Type_of_product NVARCHAR(20) NOT NULL,
+    название_изделия NVARCHAR(100) NOT NULL,
+    вид_изделия NVARCHAR(20) NOT NULL,
     Workshop_ID INT NOT NULL,
     FOREIGN KEY (Workshop_ID) REFERENCES Workshop(ID)
 );
 
 CREATE TABLE Contract (
     ID INT IDENTITY(1,1) PRIMARY KEY,
-    data DATE NOT NULL DEFAULT GETDATE(),
-    statuse NVARCHAR(20) NOT NULL,
-    Price MONEY NOT NULL CHECK (Price > 0),
+    дата DATE NOT NULL DEFAULT GETDATE(),
+    статус NVARCHAR(20) NOT NULL,
+    стоимость MONEY NOT NULL CHECK (стоимость > 0),
     Client_ID INT NOT NULL,
     Jewelery_ID INT NOT NULL UNIQUE,
     Discount_ID INT NOT NULL,
