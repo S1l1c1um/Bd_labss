25210762948739e28652c7842cea54757448afe4
eds1 <99983313+S1l1c1um@users.noreply.github.com>
2025-10-03 00:14:40 +0300

    Update SQLQuery1.sql

--- JeweleryStore-database/SQLQuery1.sql
+++ JeweleryStore-database/SQLQuery1.sql
@@ -1,21 +1,19 @@
-USE JeweleryStore;
-
 CREATE TABLE Discount (
     ID INT IDENTITY(1,1) PRIMARY KEY,
-    íàèìåíîâàíèå NVARCHAR(20) NOT NULL,
-    ðàçìåð INT NOT NULL CHECK (ðàçìåð >= 0 AND ðàçìåð <= 100)
+    Ã­Ã Ã¨Ã¬Ã¥Ã­Ã®Ã¢Ã Ã­Ã¨Ã¥ NVARCHAR(20) NOT NULL,
+    Ã°Ã Ã§Ã¬Ã¥Ã° INT NOT NULL CHECK (Ã°Ã Ã§Ã¬Ã¥Ã° >= 0 AND Ã°Ã Ã§Ã¬Ã¥Ã° <= 100)
 );
 
 CREATE TABLE Client (
     ID INT IDENTITY(1,1) PRIMARY KEY,
-    ÔÈÎ NVARCHAR(100) NOT NULL,
-    Àäðåñ NVARCHAR(100),
-    òåëåôîí NVARCHAR(20)
+    Ã”ÃˆÃŽ NVARCHAR(100) NOT NULL,
+    Ã€Ã¤Ã°Ã¥Ã± NVARCHAR(100),
+    Ã²Ã¥Ã«Ã¥Ã´Ã®Ã­ NVARCHAR(20)
 );
 
 CREATE TABLE Workshop (
     ID INT IDENTITY(1,1) PRIMARY KEY,
-    àäðåñ NVARCHAR(100) NOT NULL 
+    Ã Ã¤Ã°Ã¥Ã± NVARCHAR(100) NOT NULL 
 );
 
 CREATE TABLE Material (
@@ -24,17 +22,17 @@ CREATE TABLE Material (
 
 CREATE TABLE Jewelery (
     ID INT IDENTITY(1,1) PRIMARY KEY,
-    íàçâàíèå_èçäåëèÿ NVARCHAR(100) NOT NULL,
-    âèä_èçäåëèÿ NVARCHAR(20) NOT NULL,
+    Ã­Ã Ã§Ã¢Ã Ã­Ã¨Ã¥_Ã¨Ã§Ã¤Ã¥Ã«Ã¨Ã¿ NVARCHAR(100) NOT NULL,
+    Ã¢Ã¨Ã¤_Ã¨Ã§Ã¤Ã¥Ã«Ã¨Ã¿ NVARCHAR(20) NOT NULL,
     Workshop_ID INT NOT NULL,
     FOREIGN KEY (Workshop_ID) REFERENCES Workshop(ID)
 );
 
 CREATE TABLE Contract (
     ID INT IDENTITY(1,1) PRIMARY KEY,
-    äàòà DATE NOT NULL DEFAULT GETDATE(),
-    ñòàòóñ NVARCHAR(20) NOT NULL,
-    ñòîèìîñòü MONEY NOT NULL CHECK (ñòîèìîñòü > 0),
+    Ã¤Ã Ã²Ã  DATE NOT NULL DEFAULT GETDATE(),
+    Ã±Ã²Ã Ã²Ã³Ã± NVARCHAR(20) NOT NULL,
+    Ã±Ã²Ã®Ã¨Ã¬Ã®Ã±Ã²Ã¼ MONEY NOT NULL CHECK (Ã±Ã²Ã®Ã¨Ã¬Ã®Ã±Ã²Ã¼ > 0),
     Client_ID INT NOT NULL,
     Jewelery_ID INT NOT NULL UNIQUE,
     Discount_ID INT NOT NULL,
@@ -55,4 +53,5 @@ CREATE TABLE Materializdelie(
     Jewelery_ID INT NOT NULL,
     FOREIGN KEY (Material_ID) REFERENCES Material(ID),
     FOREIGN KEY (Jewelery_ID) REFERENCES Jewelery(ID)
+
 );
